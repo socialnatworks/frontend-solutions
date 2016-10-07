5a9c1bc7000910eafb6663f80128d1ce736fdafc
Toan Nguyen <toan2406@gmail.com>
2016-10-07 11:34:04 +0700

    Update strings.js

--- values-types-and-operators/strings.js
+++ values-types-and-operators/strings.js
@@ -1,6 +1,7 @@
 /*
  * Hello World!
- * Write a program that greets the user by name, or by saying "Hello, World!" if no name is given.
+ * Write a program that greets the user by name, eg: "Hello, Kelvin!";
+ * or by saying "Hello, World!" if no name is given.
  */
 
 exports.hello = function (name) {
